63e50c2100096c5e57678897bceef823f0dda6b1
Wisse Voortman <w_voortman@hotmail.com>
2019-11-19 14:29:38 +0100

    added bottem tab navigator

--- App.js
+++ App.js
@@ -1,21 +1,126 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements'
+import { Text, Button, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      < Button title='My First button' icon={{name: 'code'}}/>
-    </View>
-  );
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons'; // 6.2.2
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home screen!</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class NFCScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>NFC screen!</Text>
+      </View>
+    );
+  }
+}
+
+class MapScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>map screen!</Text>
+      </View>
+    );
+  }
+}
+
+class ClampManagementScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Clamp screen!</Text>
+      </View>
+    );
+  }
+}
+
+class IconWithBadge extends React.Component {
+  render() {
+    const { name, badgeCount, color, size } = this.props;
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Ionicons name={name} size={size} color={color} />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              // /If you're using react-native < 0.57 overflow outside of the parent
+              // will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const HomeIconWithBadge = props => {
+  // TODO: pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+  return <IconWithBadge {...props} badgeCount={0} />;
+};
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === 'Home') {
+    iconName = `md-home`;
+    // We want to add badges to home tab icon
+    IconComponent = HomeIconWithBadge;
+  }
+  else if (routeName === 'NFC') {
+    iconName = `md-qr-scanner`;
+  }
+  else if (routeName === 'Map') {
+    iconName = `md-map`;
+  }
+  else if (routeName === 'Clamps') {
+    iconName = `md-options`;
+  }
+  // You can return any component that you like here!
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+export default createAppContainer(
+  createBottomTabNavigator(
+    {
+      Home: { screen: HomeScreen },
+      NFC: { screen: NFCScreen },
+      Map: { screen: MapScreen },
+      Clamps: { screen: ClampManagementScreen }
+    },
+    {
+      defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) =>
+          getTabBarIcon(navigation, focused, tintColor),
+      }),
+      tabBarOptions: {
+        activeTintColor: 'darkcyan',
+        inactiveTintColor: 'lightgray',
+      },
+    }
+  )
+);
